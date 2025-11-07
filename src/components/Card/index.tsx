7c51a18336cb431ef68a815666c1961094eaf451
shakyasimha <sushakya235@proton.me>
2025-11-07 13:40:08 +0545

    finished animating the buttons

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,6 +1,6 @@
 // Component for business-card like portfolio intro section
 import { saira } from "@/ui/fonts";
-import { Github, Linkedin } from "lucide-react";
+import { Mail, Github, Linkedin } from "lucide-react";
 import Link from "next/link";
 
 export default function Card() {
@@ -8,17 +8,30 @@ export default function Card() {
     <>
       <div className="rounded-2xl shadow-lg bg-[#FFE4E1] dark:bg-[#AED6FF] mt-32 w-180 h-100 p-4">
         <div className={`${saira.className} mt-4 ml-4 text-[#333]`}>
-          <div className="text-2xl">Sushovan Shakya</div>
+          <div className="text-3xl">Sushovan Shakya</div>
           <div className="mt-2 text-xl">Full Stack Developer</div>
           <div className="mt-2 text-lg">Kathmandu, Nepal</div>
-          <div className="mt-32">{""}</div>
-          <div className="mt-2">Contact Me</div>
-          <div className="mt-4 flex flex-row gap-4">
-            <Link href="https://github.com/steinderace">
-              <Github className="text-[#333] transition duration-300 hover:text-[#222]" />
+          <div className="mt-48">{""}</div>
+          <div className="mt-4 flex flex-row gap-2">
+            <Link
+              href="mailto:reach.sushovan@gmail.com"
+              className="p-2 rounded-full transition duration-300 text-[#333] hover:bg-white active:bg-white"
+            >
+              <Mail />
             </Link>
-            <Link href="https://www.linkedin.com/in/steinderace/">
-              <Linkedin className="text-[#333] transition duration-300 hover:text-[#222]" />
+            <Link
+              href="https://github.com/steinderace"
+              target="_blank"
+              className="p-2 rounded-full transition duration-300 text-[#333] hover:text-white hover:bg-[#24292e] active:bg-[#24292e]"
+            >
+              <Github />
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/steinderace/"
+              target="_blank"
+              className="p-2 rounded-full transition duration-300 text-[#333] hover:text-white hover:bg-[#0077b5] active:bg-[#0077b5]"
+            >
+              <Linkedin />
             </Link>
           </div>
         </div>
